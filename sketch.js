4b5ce65f80750e5bde855b4160b08845e62518b1
Spacenasa2006 <71498100+Spacenasa2006@users.noreply.github.com>
2020-09-28 17:55:30 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,38 @@
+var bullet,wall;
+var thickness,speed,weight;
+
+function setup() {
+  createCanvas(1600,400);
+  bullet=createSprite(50, 200, 50, 20);
+  wall=createSprite(1200, 200, thickness, height/2);
+  wall.shapeColor=color(80,80,80);
+  thickness=random(22,83);
+  speed=random(223,321);
+  weight=random(30,52);
+  bullet.velocityX = speed;
+}
+
+function draw() {
+  background("cyan");
+  if (hascollided(bullet,wall)){
+    bullet.velocityX=0;
+    var damage=0.5*weight*speed*speed/(thickness*thickness*thickness);
+    if(damage>10){
+      wall.shapeColor=color(255,0,0);
+    }
+    if(damage<10){
+      wall.shapeColor=color(0,255,0);
+    }
+  }
+  drawSprites();
+}
+function hascollided(lbullet,lwall) {
+   bulletRightEdge=lbullet.x+lbullet.width;
+   wallLeftEdge=lwall.x;
+   if(bulletRightEdge>=wallLeftEdge){
+     return true;
+   }
+   else{
+     return false;
+   }
+}
